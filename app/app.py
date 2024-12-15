a721e6add80261e2af56100b0eaa60a68e049e68
Kyle-Myre <simoarmymen9@gmail.com>
2024-12-15 15:20:38 +0100

    Migrating Flask To Gtk

--- app/app.py
+++ app/app.py
@@ -1,9 +1,1 @@
-from flask import Flask
-from flask import render_template
-
-app = Flask(__name__)
-app.config.from_pyfile('config.py')
-
-@app.get('/')
-def index():
-    return render_template('home.html')
+import gi
