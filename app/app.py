f624836d584e125a2d0596758a027b60f81fe59e
AmbitiousFlow <mohamed.lafrouh@gmail.com>
2025-06-27 14:21:44 +0100

    Refactor download functionality: connect UI actions to controller methods and enhance download logic with error handling

--- app/app.py
+++ app/app.py
@@ -19,11 +19,36 @@ class App(QMainWindow):
 
         self.controller = DownloadController(self.download_service)
 
-        self.set_actions()
+        self.ui.PathButton.clicked.connect(self.set_path)
+
+        self.ui.ConvertButton.clicked.connect(self.download)
 
     
-    def set_actions(self):
-        self.ui.PathButton.clicked.connect(self.controller.set_path)
-        print(self.controller.get_path())
-        
-       
+    def set_path(self):
+        """
+        Sets the download path for the media files.
+        Opens a file dialog to select the directory.
+        """
+        self.controller.set_path()
+        self.ui.PathLineEdit.setText(self.controller.get_path())
+
+    def download(self):
+        """
+        Initiates the download of media files based on user input.
+        Retrieves the URL and format from the UI and calls the controller to perform the download.
+        """
+        url = self.ui.UrlTextInput.text()
+        media_format = self.ui.MediaTypeComboBox.currentText()
+        destination = self.controller.get_path()
+
+        if not url:
+            self.ui.StatusLabel.setText("Please enter a valid URL.")
+            return
+
+        try:
+
+            self.controller.download(url, media_format, destination)
+            self.ui.LogsTextEdit.append(f"Downloading {media_format} from {url} to {destination}")
+
+        except Exception as e:
+            ...
--- app/utils/download.py
+++ app/utils/download.py
@@ -1,5 +1,12 @@
 from yt_dlp import YoutubeDL
 
+import os
+
+current_dir = os.path.dirname(__file__)
+
+ffmpeg_path = os.path.abspath(os.path.join(current_dir, '..', 'libs', 'ffmpeg', 'bin', 'ffmpeg.exe'))
+
+
 def download_video(url : str): 
     
     yt = YoutubeDL()
@@ -10,7 +17,7 @@ def download_audio(url: str):
     options = {
         "format": "bestaudio/best",
         "outtmpl": "%(title)s.%(ext)s",
-        "ffmpeg_location": r"D:\\Work\\Dandilion\\bin\\ffmpeg.exe",
+        "ffmpeg_location": ffmpeg_path,
         "postprocessors": [
             {
                 "key": "FFmpegExtractAudio",
